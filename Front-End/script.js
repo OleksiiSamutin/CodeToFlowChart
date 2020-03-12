55f4e033e392cd6c9eee16e50b517c344259aad3
OleksiiSamutin <samutin63@gmail.com>
2020-03-12 17:11:33 +0200

    Added webpack to package.json

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,7 +1,9 @@
+let backend = require('../template/src/Main.js');
 let input = document.getElementById("inputFile");
 input.addEventListener('change', sendFileToBackend);
 
 function sendFileToBackend(){
     var currFiles = input.value;
     console.log(currFiles);
+    backend.setSourceCode(currFiles);
 }
