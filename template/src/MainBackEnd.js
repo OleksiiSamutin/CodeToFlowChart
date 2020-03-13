b5c0f5024401b80ee06ec6c7aca0de31fd5ae25f
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-03-13 17:42:47 +0200

    Make one Main

--- template/src/MainBackEnd.js
+++ template/src/MainBackEnd.js
@@ -1,11 +1,16 @@
 const ParserC = module.require("./Parsers/ParserC.js");
-module.exports.setSourceCode = function setSourceCode(path, file) {
+function setSourceCode(path) {
   // This operation below read code from Testing File and split it line by line, it's needed
 
-  console.log(path);
   //  only for making a start point and will not be used in a future
 
   // @param fs  -- It's shortcut of FileSystem
+  text = require("fs")
+    .readFileSync(path, "ASCII")
+    .replace(/{/g, "\n{\n")
+    .replace(/}/g, "\n}\n")
+    .split("\n");
+
   // This variable below need for temporal save cut pass
   switch (path.split(".")[1]) {
     case "cpp":
@@ -16,12 +21,7 @@ module.exports.setSourceCode = function setSourceCode(path, file) {
 
       parser.print();
 
-      parser.parseToJSON(
-        file
-          .replace(/{/g, "\n{\n")
-          .replace(/}/g, "\n}\n")
-          .split("\n")
-      );
+      parser.parseToJSON(text);
       break;
     }
     case "py": {
@@ -31,4 +31,6 @@ module.exports.setSourceCode = function setSourceCode(path, file) {
       break;
     }
   }
-};
+}
+
+setSourceCode("SourceCode/Test.cpp");
