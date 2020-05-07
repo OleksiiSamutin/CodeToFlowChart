87051473810176624b254e67ac8b1e040e4a57fa
OleksiiSamutin <samutin63@gmail.com>
2020-05-07 17:19:00 +0300

    Added client-server side

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,7 +1,7 @@
 let backend = require('../template/src/Main.js');
 let input = document.getElementById("inputFile");
 input.addEventListener('change', sendFileToBackend);
-
+let file;
 function sendFileToBackend(){
     var reader = new FileReader();
     var currFiles = input.files[0];
@@ -11,7 +11,7 @@ function sendFileToBackend(){
         reader.readAsText(currFiles);
 
         reader.onload = function () {
-            let file = reader.result;
+            file = reader.result;
             backend.setSourceCode(currFiles.name, file);
           }
     }
@@ -34,3 +34,31 @@ Array.prototype.forEach.call(inputs, function(input){
       label.innerHTML = labelVal;
 	});
 });
+
+if (!window.WebSocket) {
+	document.body.innerHTML = 'WebSocket в этом браузере не поддерживается.';
+}
+
+// создать подключение
+var socket = new WebSocket("ws://localhost:8081");
+
+// отправить сообщение из формы publish
+document.forms.publish.onsubmit = function() {
+  var outgoingMessage = this.message.value;
+
+  socket.send(outgoingMessage);
+  return false;
+};
+
+// обработчик входящих сообщений
+socket.onmessage = function(event) {
+  var incomingMessage = event.data;
+  showMessage(incomingMessage);
+};
+
+// показать сообщение в div#subscribe
+function showMessage(message) {
+  var messageElem = document.createElement('div');
+  messageElem.appendChild(document.createTextNode(message));
+  document.getElementById('subscribe').appendChild(messageElem);
+}
